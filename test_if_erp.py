f1956a4b8f78603cd03dd6741c19209c242c04ae
Etienne1209 <etienne.jugeur1209@gmail.com>
2025-03-04 13:36:20 +0100

    MAJ @IP

--- test_if_erp.py
+++ test_if_erp.py
@@ -1,5 +1,5 @@
-erp_ipaddr = "192.168.0.17"
-erp_port = "8069"
+erp_ipaddr = "172.31.10.137"
+erp_port = "8026"
 erp_url = f'http://{erp_ipaddr}:{erp_port}'
 import xmlrpc.client
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(erp_url))
