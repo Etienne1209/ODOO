2915a17d12b3f1805e04b806be3f055c588c7837
Etienne1209 <etienne.jugeur1209@gmail.com>
2025-03-04 14:00:11 +0100

    MAJ

--- test_if_erp.py
+++ test_if_erp.py
@@ -8,8 +8,13 @@ import xmlrpc.client
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(erp_url))
 version = common.version()
 user_id = common.authenticate(erp_db,erp_user,erp_pwd,{})
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(erp_url))
+access = models.execute_kw(erp_db,user_id, erp_pwd,
+    'mrp.production','check_access_rights',
+    ['write'],{'raise_exception': False})
 
 print ("Connexion ODOO")
 print(f"@URL={erp_url}")
 print(f"Odoo version={version['server_serie']}")
 print(f"Odoo authentification:{user_id}")
+print(f"Manufactoring Order write access rights : {access}")
