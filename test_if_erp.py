d82deb2b122130720a3e7e5cc52b12e2ede2cdbd
Etienne1209 <etienne.jugeur1209@gmail.com>
2025-03-04 14:58:45 +0100

    MAJ

--- test_if_erp.py
+++ test_if_erp.py
@@ -1,23 +1,26 @@
+import xmlrpc.client
 #----------------------------------
 #Etablir la connexion avec Odoo
 #----------------------------------
 erp_ipaddr = "172.31.10.137"
 erp_port = "8026"
 erp_url = f'http://{erp_ipaddr}:{erp_port}'
-import xmlrpc.client
-common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(erp_url))
-version = common.version()
 
 print ("Connexion ODOO")
 print(f"@URL={erp_url}")
-print(f"Odoo version={version['server_serie']}")
 
+try:
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(erp_url))
+    version = common.version()
+    print(f"Odoo version={version['server_serie']}")
+except ConnectionRefusedError:
+    print("Odoo Server not found or connection rejected")
 #---------------------------------
 #Athentification avec Odoo
 #---------------------------------
 erp_db = "YOURT"
 erp_user = "etienne.jugeur1209@gmail.com"
-erp_pwd = "vxrq-rwjs-ejpz"
+erp_pwd = "mauvais"
 user_id = common.authenticate(erp_db,erp_user,erp_pwd,{})
 
 print(f"Odoo authentification:{user_id}")
