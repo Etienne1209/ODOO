12bf2f83f8d3651508c0d3eceec0dcb385d0c0b7
Etienne1209 <etienne.jugeur1209@gmail.com>
2025-03-04 10:56:48 +0100

    MAJ

--- test_if_erp.py
+++ test_if_erp.py
@@ -2,7 +2,9 @@ erp_ipaddr = "192.168.0.17"
 erp_port = "8069"
 erp_url = f'http://{erp_ipaddr}:{erp_port}'
 import xmlrpc.client
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(erp_url))
+version = common.version()
 
 print ("Connexion ODOO")
 print(f"@URL={erp_url}")
-
+print(f"Odoo version={version}")
