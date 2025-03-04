09610a948ccaff21f80c11a964e3a0f6f275b476
Etienne1209 <etienne.jugeur1209@gmail.com>
2025-03-04 13:49:42 +0100

    MAJ

--- test_if_erp.py
+++ test_if_erp.py
@@ -1,10 +1,15 @@
 erp_ipaddr = "172.31.10.137"
 erp_port = "8026"
 erp_url = f'http://{erp_ipaddr}:{erp_port}'
+erp_db = "YOURT"
+erp_user = "etienne.jugeur1209@gmail.com"
+erp_pwd = "vxrq-rwjs-ejpz"
 import xmlrpc.client
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(erp_url))
 version = common.version()
+user_id = common.authenticate(erp_db,erp_user,erp_pwd,{})
 
 print ("Connexion ODOO")
 print(f"@URL={erp_url}")
 print(f"Odoo version={version['server_serie']}")
+print(f"Odoo authentification:{user_id}")
